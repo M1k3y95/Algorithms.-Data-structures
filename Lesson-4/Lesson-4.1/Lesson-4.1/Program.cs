d998b49f461f4ba43088aeaec621ef5a782d2df5
M1k3y95 <rockheart95@gmail.com>
2021-10-17 17:49:37 +0500

    Исправления: реализован поиск на одинаковых данных

--- Lesson-4/Lesson-4.1/Lesson-4.1/Program.cs
+++ Lesson-4/Lesson-4.1/Lesson-4.1/Program.cs
@@ -4,90 +4,114 @@ using System.Diagnostics;
 
 namespace Lesson_4._1
 {
-    class ArraySearch
+    /// <summary>
+    /// Класс, хранящий и заполняющий массив с рандомными строками
+    /// </summary>
+    class RandomStringArray
     {
-        private string[] MyArray { get; set; }
+        public int MyArraySize { get; set; }
 
-        public ArraySearch(int arraySize)
+        public RandomStringArray(int arraySize)
         {
-            MyArray = new string[arraySize];
-            Search(MyArray);
+            MyArraySize = arraySize;
         }
 
-        private string[] FillArray(string[] array)
+        /// <summary>
+        /// Заполнение массива уникальными рандомными строками
+        /// </summary>
+        /// <returns>Массив указанного размера, заполненный уникальными случайными строками</returns>
+        public string[] FillArray()
         {
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = Guid.NewGuid().ToString();
-            }
-            return array;
-        }
+            string[] coreArray = new string[MyArraySize];
 
-        private string Search(string[] array)
-        {
-            Stopwatch sw = new();
-            FillArray(array);
-            string searchValue = array[array.Length - 100];
-
-            sw.Start();
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < coreArray.Length; i++)
             {
-                if (array[i] == searchValue)
-                {
-                    Console.WriteLine("Строка найдена");
-                    sw.Stop();
-                    Console.WriteLine($"Время поиска в массиве {sw.ElapsedMilliseconds}мс");
-                    return array[i];
-                }
+                coreArray[i] = Guid.NewGuid().ToString();
             }
-            sw.Stop();
-            Console.WriteLine("Искомой строки не существует!");
-            Console.WriteLine($"Время поиска в массиве {sw.ElapsedMilliseconds}мс");
-            return null;
+
+            return coreArray;
         }
     }
 
-    class HashSetSearch
+    /// <summary>
+    /// Класс, содержащий сущности для поиска искомого значения в массиве
+    /// </summary>
+    class ArraySearch
     {
 
-        public HashSetSearch(int size)
+        public ArraySearch(string[] coreArray, string requiredString)
         {
-            Search(size);
+
+            Search(coreArray, requiredString);
         }
 
-        private HashSet<string> FillHashSet(HashSet<string> someList, int size)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                if (i == size - 5)
-                {
-                    someList.Add("Найди меня");
-                    continue;
-                }
-                someList.Add(Guid.NewGuid().ToString());
-            }
-            return someList;
-        }
-
-        private void Search(int size)
+        /// <summary>
+        /// Поиск строки в массиве
+        /// </summary>
+        /// <param name="coreArray">исходный массив</param>
+        /// <param name="requiredString">искомая строка</param>
+        private void Search(string[] coreArray, string requiredString)
         {
             Stopwatch sw = new();
-            HashSet<string> someList = new();
-            someList = FillHashSet(someList, size);
 
             sw.Start();
-            if (someList.Contains("Найди меня"))
+            for (int i = 0; i < coreArray.Length; i++)
             {
-                Console.WriteLine("Строка найдена");
-                sw.Stop();
-                Console.WriteLine($"Время поиска в HashSet {sw.ElapsedMilliseconds}мс");
+                if (coreArray[i] == requiredString)
+                {
+                    Console.WriteLine("Искомая строка найдена в массиве!");
+                    break;
+                }
             }
-            else
+            sw.Stop();
+            Console.WriteLine($"Время поиска в массиве: {sw.ElapsedMilliseconds}мс\t");
+        }
+    }
+
+    /// <summary>
+    /// Класс, содержащий сущности для поиска искомой строки в HashSet-коллекции
+    /// </summary>
+    class HashSetSearch
+    {
+        public HashSet<string> MyHashSetList { get; set; }
+        public string RequiredString { get; set; }
+
+        public HashSetSearch(string[] coreArray, string requiredString)
+        {
+            RequiredString = requiredString;
+            FillHashSet(coreArray);
+            Search();
+        }
+
+        /// <summary>
+        /// Заполнение коллекции на основе исходного массива
+        /// </summary>
+        /// <param name="coreArray">исходный массив</param>
+        private void FillHashSet(string[] coreArray)
+        {
+            MyHashSetList = new HashSet<string>();
+
+            for (int i = 0; i < coreArray.Length; i++)
             {
-                Console.WriteLine("Строка не найдена");
-                sw.Stop();
-                Console.WriteLine($"Время поиска в HashSet {sw.ElapsedMilliseconds}мс");
+                MyHashSetList.Add(coreArray[i]);
             }
+        }
+
+        /// <summary>
+        /// Поиск наличия искомой строки в HashSet-коллекции
+        /// </summary>
+        private void Search()
+        {
+            Stopwatch sw = new();
+
+            sw.Start();
+            if (MyHashSetList.Contains(RequiredString))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Искомая строка найдена в HashSet!");
+            }
+            sw.Stop();
+            Console.WriteLine($"Время поиска в HashSet: {sw.ElapsedMilliseconds}мс"); // почему всегда 0?
         }
     }
 
@@ -96,12 +120,16 @@ namespace Lesson_4._1
         static void Main(string[] args)
         {
             int number = 10_000_000;
+            Random random = new();
 
-            ArraySearch arraySearch = new ArraySearch(number);
+            RandomStringArray randomArr = new(number);
 
-            Console.WriteLine();
+            string[] coreArray = randomArr.FillArray();
+            string requiredString = coreArray[random.Next(0, number - 1)];
 
-            HashSetSearch hashSetSearch = new HashSetSearch(number);
+            ArraySearch arSearch = new(coreArray, requiredString);
+
+            HashSetSearch hsSearch = new(coreArray, requiredString);
         }
     }
 }
